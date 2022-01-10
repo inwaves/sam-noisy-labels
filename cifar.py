772d9d5b6354646fa12bc4b44f588cb3727d09f6
Andrei <inwaves@live.com>
2022-01-10 17:16:34 +0000

    Ready to train small net with SAM

---
+++ cifar.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+class Cifar:
+    def __init__(self, batch_size, threads):
+        data_transforms = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=data_transforms)
+        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=data_transforms)
+
+        self.train = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=threads)
+        self.test = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=threads)
+
+        self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
---
+++ train.py
@@ -0,0 +1,85 @@
+import torch
+
+from smooth_cross_entropy import smooth_crossentropy
+from utility.log import Log
+from utility.step_lr import StepLR
+from utility.bypass_bn import enable_running_stats, disable_running_stats
+from sam import SAM
+from net import Net
+from cifar import Cifar
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Constants.
+batch_size = 4
+epochs = 10
+
+# Hyperparameters.
+rho = 0.05
+adaptive = False
+lr = 0.1
+momentum = 0.9
+weight_decay = 0.0005
+epochs = 200
+label_smoothing = 0.1
+
+log = Log(log_each=10)
+model = Net()
+base_optimizer = torch.optim.SGD
+optimizer = SAM(model.parameters(), base_optimizer, rho=rho, adaptive=adaptive, lr=lr, momentum=momentum,
+                weight_decay=weight_decay)
+scheduler = StepLR(optimizer, lr, epochs) # Learning rate scheduler.
+dataset = Cifar(batch_size, 0)
+
+
+def train():
+    for epoch in range(epochs):
+        # Set the model to training mode.
+        model.train()
+        log.train(len_dataset=len(dataset.train))
+
+        # For CIFAR-10 we expect images of size 3x32x32.
+
+        # Iterate over the batches in the training set.
+        for batch in dataset.train:
+            inputs, targets = (b.to(device) for b in batch)
+            # print(inputs.shape)
+
+            # first forward-backward step
+            enable_running_stats(model)
+            predictions = model(inputs)
+            loss = smooth_crossentropy(predictions, targets, smoothing=label_smoothing)
+            loss.mean().backward()
+            optimizer.first_step(zero_grad=True)
+
+            # second forward-backward step
+            disable_running_stats(model)
+            smooth_crossentropy(model(inputs), targets, smoothing=label_smoothing).mean().backward()
+            optimizer.second_step(zero_grad=True)
+
+            with torch.no_grad():
+                correct = torch.argmax(predictions.data, 1) == targets
+                log(model, loss.cpu(), correct.cpu(), scheduler.lr())
+                scheduler(epoch)
+
+        # Set the model to eval mode.
+        model.eval()
+        log.eval(len_dataset=len(dataset.test))
+
+        # Begin testing the model on the test set.
+        with torch.no_grad():
+            for batch in dataset.test:
+                inputs, targets = (b.to(device) for b in batch)
+
+                predictions = model(inputs)
+                loss = smooth_crossentropy(predictions, targets)
+                correct = torch.argmax(predictions, 1) == targets
+                log(model, loss.cpu(), correct.cpu())
+
+        log.flush()
+
+
+if __name__ == '__main__':
+    train()
+
+
