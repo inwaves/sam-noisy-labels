69d73055657645df2f5c98884db55cf87187b3eb
Andrei <inwaves@live.com>
2022-01-19 10:31:05 +0000

    Fix python command in scripts to use conda's dist

--- scripts/mixed_experiments.sh
+++ scripts/mixed_experiments.sh
@@ -1,3 +1,4 @@
+conda activate base
 python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --adaptive=False --label_type="worse" --epochs=200
 python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --adaptive=False --label_type="blue" --noise=0.4 --epochs=200
 python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --rho_scheduler="stepdecay" --adaptive=False --label_type="worse"  --epochs=200
