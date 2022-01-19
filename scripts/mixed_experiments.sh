e59fa47bfd943c0eb40e788ed9c6dc4de3a72688
Andrei <inwaves@live.com>
2022-01-19 10:28:54 +0000

    Fix python command in scripts to use conda's dist

--- scripts/mixed_experiments.sh
+++ scripts/mixed_experiments.sh
@@ -1,9 +1,9 @@
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --adaptive=False --label_type="worse" --epochs=200
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --adaptive=False --label_type="blue" --noise=0.4 --epochs=200
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --rho_scheduler="stepdecay" --adaptive=False --label_type="worse"  --epochs=200
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --rho_scheduler="stepdecay" --adaptive=False --label_type="blue" --noise=0.4 --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --adaptive=False --label_type="worse" --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --adaptive=False --label_type="blue" --noise=0.4 --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --rho_scheduler="stepdecay" --adaptive=False --label_type="worse"  --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=0.1 --rho_scheduler="stepdecay" --adaptive=False --label_type="blue" --noise=0.4 --epochs=200
 
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --adaptive=True --label_type="worse" --epochs=200
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --adaptive=True --label_type="blue" --noise=0.4 --epochs=200
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --rho_scheduler="stepdecay" --adaptive=True --label_type="worse"  --epochs=200
-python3 train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --rho_scheduler="stepdecay" --adaptive=True --label_type="blue" --noise=0.4 --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --adaptive=True --label_type="worse" --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --adaptive=True --label_type="blue" --noise=0.4 --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --rho_scheduler="stepdecay" --adaptive=True --label_type="worse"  --epochs=200
+python train.py --dataset "cifar10" --optimiser-choice "SAM" --initial_rho=2.0 --rho_scheduler="stepdecay" --adaptive=True --label_type="blue" --noise=0.4 --epochs=200
